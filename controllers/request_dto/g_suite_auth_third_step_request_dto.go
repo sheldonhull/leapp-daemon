95b66b3c9ad06d07cd6c4d1e727af37b4d2c2dd1
Eric Villa <eric.villa91@gmail.com>
2021-03-01 17:00:17 +0100

    Implemented GSuite SAML authentication flow; Implemented WebSocket POC

---
+++ controllers/request_dto/g_suite_auth_third_step_request_dto.go
@@ -0,0 +1,18 @@
+package request_dto
+
+import (
+	"github.com/gin-gonic/gin"
+	"leapp_daemon/custom_errors"
+)
+
+type GSuiteAuthThirdStepRequestDto struct {
+	IsMfaTokenRequested bool `json:"isMfaTokenRequested"`
+	ResponseForm string `json:"responseForm"`
+	SubmitURL string `json:"submitURL"`
+	Token string `json:"token"`
+}
+
+func (requestDto *GSuiteAuthThirdStepRequestDto) Build(context *gin.Context) error {
+	err := custom_errors.NewBadRequestError(context.ShouldBindJSON(requestDto))
+	return err
+}
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"leapp_daemon/controllers"
 	"leapp_daemon/engine"
 	"leapp_daemon/logging"
 )
 
 func main() {
+	go controllers.Hub.Run()
+
 	defer logging.CloseLogFile()
 	eng := engine.Engine()
 	eng.Serve(8080)
