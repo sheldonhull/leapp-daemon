db4a61b561d7a7306fe0a61e098bcd2f4a12b8f3
Nicolo <nicolo@besharp.it>
2021-04-12 11:23:43 +0200

    removed commented code

--- api/controller/custom_validator/aws_region_validator.go
+++ api/controller/custom_validator/aws_region_validator.go
@@ -5,11 +5,6 @@ import (
 	"leapp_daemon/core/aws/region"
 )
 
-//
-//var awsRegionValidator validator.Func = func(fl validator.FieldLevel) bool {
-//  return region.IsRegionValid(fl.Field().String())
-//}
-
 func AwsRegionValidator(fl validator.FieldLevel) bool {
 	return region.IsRegionValid(fl.Field().String())
 }
